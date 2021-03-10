0baa59aa18a0efbc218d0d99d0a4b6d87ae4867b
EdinCehajic <ed.cehajic@gmail.com>
2021-03-10 16:40:04 +0100

    Adding Day03

---
+++ ex02/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+    // Only change code below this line
+
+    // Komentar sa jednom linijom
+
+    /* Komentar sa vise linija
+    koji pokazuje da mozemo u vise linija pisati 
+    primjer ovdje je sa  3 linije */
+
+    // Only change code above this line
+    }
+    console.log(main());
+    module.exports = main;
